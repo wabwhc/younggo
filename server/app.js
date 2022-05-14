2486961c6688243416965b4fc2ebcc463a472f37
Chanwool <kd04173@gmail.com>
2022-05-14 23:55:56 +0900

    app.js 수정

--- server/app.js
+++ server/app.js
@@ -8,21 +8,21 @@ const passport = require('passport')
 const path = require('path')
 const router = require('./api/router')
 const conn = require('./mysqlconn');
-const nunjucks  = require('nunjucks');
+const nunjucks = require('nunjucks');
 //회원가입부분 라우터로 처리 길어질듯 해서
 const signupR = require('./signup');
 const e = require('connect-flash');
 //미들웨어
-app.use(express.urlencoded({extended:false}))
-app.use(session({ 
-    secret: '비밀코드', resave: true, saveUninitialized: false 
+app.use(express.urlencoded({extended: false}))
+app.use(session({
+    secret: '비밀코드', resave: true, saveUninitialized: false
 })); // 세션 활성화
 app.use(passport.initialize()); // passport 구동
 app.use(passport.session()); // 세션 연결
 app.use('/api', router)
 app.use('/signup', signupR);
 
-app.set('view engine', 'njk');
+app.set('view engine', 'html');
 nunjucks.configure('views', {
     express: app,
     autoescape: true,
@@ -39,23 +39,24 @@ app.get('/', (req, res) => {
     res.render('main.html');
 })
 app.get('/main', (req, res) => {
-    let  isLogin
-    if(req.user === undefined){
+    let isLogin
+    if (req.user === undefined) {
         isLogin = false;
-    }else{
+    } else {
         isLogin = true;
     }
-    res.render('main.html', {useremail : req.user, isLogin : isLogin})
+
+    res.render('main.html', {title: '메인페이지', username: req.body.username, isLogin: isLogin})
 })
 
 app.get('/profile', (req, res) => {
-    if(req.user === undefined){
+    if (req.user === undefined) {
         res.redirect('/login')
-    }else{
+    } else {
         let sql = 'select * from users where useremail = ?'
-        conn.query(sql, [req.user],(err, result, filed) => {
+        conn.query(sql, [req.user], (err, result, filed) => {
             console.log(result)
-            res.render('profile.html', {user : result[0]});
+            res.render('profile.html', {title: '프로필', user: result[0]});
         })
     }
 })
@@ -63,23 +64,31 @@ app.get('/profile', (req, res) => {
 app.get('/board', (req, res) => {
     let sql = 'select article_id, article_title, useremail, category from articles'
     conn.query(sql, (err, result, filed) => {
-        res.render('board.html', {article : result});
+        res.render('board.html', {title: '게시판', article: result});
     })
 })
 
 app.post('/login', passport.authenticate('local', {
-    successRedirect:'/main',
+    successRedirect: '/main',
     failureRedirect: '/login',
     failureFlash: true
 }))
 
 app.get('/login', (req, res) => {
-    if(req.user === undefined){
-        res.render('login.html',{ message : req.flash("error")});
-    }else{
+    if (req.user === undefined) {
+        res.render('login.html', {title: '로그인', message: req.flash("error")});
+    } else {
         res.redirect('/main')
     }
 })
+
+const {isLoggedIn, isNotloggedIn} = require('./middlewares');
+
+app.get('/logout', isLoggedIn, (req, res) => {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/main');
+});
 
 app.get('/subjects', (req, res) => {
 
