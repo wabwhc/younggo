82fed861efc77c19d41cafa2d5a25b2af1414cb9
KIMUHYUN <rladngus5486@gamil.com>
2022-05-30 19:56:41 +0900

    board update

--- git.md
+++ git.md
@@ -1,2 +1,8 @@
 git init
 git remote add origin 주소
+
+
+데이터 10개씩 보내기
+데이터 시간순으로 보내기
+관리자인지 아닌지 보내기
+article 콘텐츠 카테고리 데이터베이스 변경
--- views/public/src/board.js
+++ views/public/src/board.js
@@ -26,6 +26,7 @@ document.querySelector('.account_title').addEventListener('click', (e) => {
     document.querySelector('.study_title').style.background = '#fff';
     document.querySelector('.account_title').style.background = '#aaa';
 });
+/* end */
 
 /* 질문 등록 */
 document.querySelector(".input_qna").addEventListener('click', (e) => {
@@ -51,6 +52,7 @@ document.querySelector("#qna_content button").addEventListener('click', async (e
         console.error(err);
     }
 });
+/* end */
 
 /* 질문 목록 */
 document.querySelectorAll("#article_body tr").forEach((el) => {
@@ -64,20 +66,51 @@ document.querySelectorAll("#article_body tr").forEach((el) => {
 const getContent = async (id) => {
     try {
         if(document.querySelector(`#article_${id}_content td`)){
+            document.querySelector(`#article_${id}_content`).style.height = '0px'
+            document.querySelector(`#article_${id}_content td`).remove();
             document.querySelector(`#article_${id}_content td`).remove();
         } else {
             const result = await axios.get(`/board/${id}/content`);
             const contents = result.data;
             const tr = document.querySelector(`#article_${id}_content`);
+            tr.style.height = '100px';
             tr.innerHTML = '';
             console.log(contents);
             contents.map(content => {
                 let td = document.createElement('td');
+                td.textContent = content.article_content;
+                tr.appendChild(td);
+                td = document.createElement('td');
+                td.colSpan = 2;
                 td.textContent = content.category;
                 tr.appendChild(td);
             });
+            tr = document.querySelector(`article_${a.article_id}_content`);
+            tr.innerHTML = '';
         }
     } catch (err) {
         console.error(err);
     }
 }
+/* end */
+
+/* 테이블 페이지 */
+document.querySelectorAll(".table_footer_list").forEach((el) => {
+    el.addEventListener('click', (e) => {
+        const id = el.querySelector('li').className;
+        reText(id)
+    });
+});
+
+const reText = async (id) => {
+    try {
+        const li = document.querySelector(`.${id}`);
+        console.log(li);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+/* end */
+
+/* axios.get($) */
